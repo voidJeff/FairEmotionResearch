0abf9137d6e6e8e05e47049739a447a1ec42b2db
Alex Fan <alexfan@stanford.edu>
2023-02-02 21:41:28 -0800

    change run_baseline

--- run_baseline.sh
+++ run_baseline.sh
@@ -1,6 +1,21 @@
-#!/usr/bin/env/bash
+#!/bin/bash
 
-#SBATCH	-o	slurm.sh.out
-#SBATCH	-p	defq
+#SBATCH -o      slurm.sh.out
+#SBATCH -p      defq
+#SBATCH --partition=k80
+#SBATCH --nodes 1
+#SBATCH --cpus-per-task 4
+#SBATCH --ntasks 1
+#SBATCH --gres=gpu:1
+#SBATCH --time=06:00:00
 
-python train.py --train_dir ./train_set --val_dir ./val_set --lr 1e-4 -- -n emotion_baseline
+module purge
+module load cuda/11.7
+
+python train.py --train_dir ./train_set --val_dir ./val_set --lr 1e-4 -n emotion_baseline_7lab
+
+
+
+
+
+
